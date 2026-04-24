0ca7346da5738681e5b6f2949ce81608b20f94cc
Nesdael <danieldurfue7@gmail.com>
2026-04-23 20:50:54 -0500

    6 actualizacion

--- prueba 2/Css/portafolio prueba 2/menu.js
+++ prueba 2/Css/portafolio prueba 2/menu.js
@@ -1,15 +1,9 @@
 
-function mostrarMenu() {
-    let menu = document.getElementById("aside");
 
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";
-        menu.style.position = "fixed";
-        menu.style.top = "0";
-        menu.style.padding = "30px 40px";
-        menu.style.flexDirection = "column";
-    } else {
-        menu.style.display = "none";
-    }
-}
+let img = document.getElementsByClassName("img-b-aside")[0]
+let menu = document.getElementById("aside");
 
+img.addEventListener("click", function(){
+    menu.classList.toggle("activo")
+});
+    
